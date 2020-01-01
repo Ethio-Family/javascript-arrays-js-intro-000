05434f8245ac9be98a63197ec02c270a259823b5
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-01 05:39:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -48,8 +48,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 // This function should return the entire array in the same line, and it should not mutate the original array.
 
 function removeElementFromBeginningOfArray(array) {
-  var newArray = new Array();
-  newArray = array
-  array.slice(1)
-  return newArray;
+  //var newArray = new Array();
+  //newArray = array
+  array = array.slice(1)
+  return array;
 }
