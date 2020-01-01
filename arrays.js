1e15907a4ed69e9616178f1e800374f7f85a053b
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-01 05:47:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -59,6 +59,15 @@ function removeElementFromBeginningOfArray(array) {
 // Your function should return the entire array, and it should mutate the array.
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.pop()
+  array = array.pop()
   return array;
+}
+
+
+//  Define a function in arrays.js called removeElementFromEndOfArray
+// that takes an array as its only argument and removes the last element.
+// Your function should return the array without the last element, and it should not mutate the original array.
+
+function removeElementFromEndOfArray(array) {
+
 }
