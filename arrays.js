2cadd06d0d421120f895b32bf10107bb6411d557
daring-array-6961 <ebrahimnery96@gmail.com>
2020-01-01 05:10:37 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var chocolateBars = [ 'snickers', 'hundred grand', 'kitkat', 'skittles' ];
+
+
+function addElementToBeginningOfArray (array, element) {
+  array.unshift(element)
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  var newArray = new Array();
+  newArray = [ ...array, element]
+  return newArray;
+}
+
+function addElementToEndOfArray(array, element) {
+  array.push(element)
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray() {
+  array.push(element)
+  var newArray = new Array();
+  newArray = [element, ...array]
+  return newArray;
+}
